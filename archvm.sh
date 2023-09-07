da851f5e724607928296fd76ba2be3b63820810f
morpheys666 <morpheys3519@gmail.com>
2023-09-07 13:20:59 +0200

    Added check for open-vm-tools

--- archvm.sh
+++ archvm.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
-#Deinstall open-vm-tools
-echo "Uninstalling open-vm-tools..."
-sudo pacman -R open-vm-tools
-sleep 2
+clear
+#Detecting if open-vm-tools are installed. If true, uinstall them, else continue
+echo "Checking for open-vm-tools..."
+if [ "$(pacman -Q | awk '/open-vm-tools/ {print }'|wc -l)" -ge 1 ]
+then
+	echo "Uninstalling open-vm-tools..."
+	sudo pacman -R open-vm-tools
+	sleep 2
+else
+	echo "open-vm-tools is not installed."
+	sleep 2
+fi
 
 #create fake /etc/init.d/rc*.d directories
 echo "Creating fake /etc/init.d/rc*.d directories..."
@@ -11,7 +19,7 @@ sleep 2
 
 #download vmware-tools
 echo "Downloading and extracting VMware Tools..."
-curl -H ‘X-Requested-With: XMLHttpRequest’ -o vmware-tools.tar.gz "https://brch1005.ddns.net/index.php/s/j49AdQQGX6tfyj7/download" | tar xvzf -
+curl -H ‘X-Requested-With: XMLHttpRequest’ -o vmware-tools.tar.gz "https://brch1005.ddns.net/index.php/s/j49AdQQGX6tfyj7/download" | tar xzf -
 sleep 2
 
 echo "Installing VMware Tools..."
