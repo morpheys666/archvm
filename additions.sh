7c72ee9a0bd33ea7344c52d43b082cbe13e0973f
morpheys666 <morpheys3519@gmail.com>
2023-09-07 15:07:40 +0200

    added changing background

--- additions.sh
+++ additions.sh
@@ -10,3 +10,8 @@ echo "Enabling autologin..."
 sudo sed -i 's/#autologin-user=/autologin-user=chris/g' /etc/lightdm/lightdm.conf
 sudo groupadd -r autologin
 sudo gpasswd -a chris autologin
+
+#Changing desktop background
+USER=$(whoami)
+cp background.jpg /home/${USER}/Pictures
+xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitor0/workspace0/last-image -s /home/${USER}/Pictures/background.jpg
