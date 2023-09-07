e0735d311bbb3a2d5ced55acbeb2a857a5b461aa
morpheys666 <138847699+morpheys666@users.noreply.github.com>
2023-09-07 11:16:53 +0200

    Add files via upload

---
+++ archvm.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#Deinstall open-vm-tools
+echo "Uninstalling open-vm-tools..."
+sudo pacman -R open-vm-tools
+sleep 2
+
+#create fake /etc/init.d/rc*.d directories
+echo "Creating fake /etc/init.d/rc*.d directories..."
+ for x in {0..6}; do sudo mkdir -p /etc/init.d/rc${x}.d; done
+sleep 2
+
+#download vmware-tools
+echo "Downloading and extracting VMware Tools..."
+curl -H ‘X-Requested-With: XMLHttpRequest’ -o vmware-tools.tar.gz "https://brch1005.ddns.net/index.php/s/j49AdQQGX6tfyj7/download" | tar xvzf -
+sleep 2
+
+echo "Installing VMware Tools..."
+sudo ./vmware-install.pl
