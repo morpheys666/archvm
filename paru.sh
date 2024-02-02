6bc672d0635c03461074efd1a87c4ebd79e6988a
morpheys666 <138847699+morpheys666@users.noreply.github.com>
2024-02-02 14:36:40 +0100

    Update paru.sh

--- paru.sh
+++ paru.sh
@@ -1,1 +1,11 @@
-
+if [ "$(pacman -Q | awk '/paru/ {print }'|wc -l)" -ge 1 ]
+then
+	echo "Already installed... Nothing to do..."
+	sleep 2
+else
+  sudo pacman -S --needed base-devel
+	git clone https://aur.archlinux.org/paru.git
+ cd paru
+ makepkg -si
+	sleep 2
+fi
